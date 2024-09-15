05c95eb91b5960b52ebc51a0b724a90ef1c0b8da
Boruman <147920803+Boruman@users.noreply.github.com>
2024-09-15 21:41:10 +0700

    ‎

---
+++ Pertemuan 3/Soal 2/KonversiSuhu.java
@@ -0,0 +1,15 @@
+package Pertemuan3.Latihan2;
+
+/**
+ *
+ * @author Syafiq
+ */
+public class KonversiSuhu {
+    public double celciusToFahrenheit(double celcius) {
+        return (celcius * 9/5) + 32;
+    }
+
+    public double celciusToReamur(double celcius) {
+        return celcius * 4/5;
+    }
+}

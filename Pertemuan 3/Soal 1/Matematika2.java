2b7e65682cf988cf8a557c466ce6a2dda02a5433
Boruman <147920803+Boruman@users.noreply.github.com>
2024-09-15 21:08:44 +0700

    ‎

---
+++ Pertemuan 3/Soal 1/Matematika2.java
@@ -0,0 +1,12 @@
+package pertemuan3.latihan1;
+
+/**
+ *
+ * @author Syafiq
+ */
+public class Matematika2 extends Matematika {
+    public int modulus(int a, int b) {
+        return a% b;
+    }
+    
+}

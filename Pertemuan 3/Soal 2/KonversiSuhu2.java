c27c3db9cfab0cccecfe42818d8f12c6eb243300
Boruman <147920803+Boruman@users.noreply.github.com>
2024-09-15 21:42:07 +0700

    ‎

---
+++ Pertemuan 3/Soal 2/KonversiSuhu2.java
@@ -0,0 +1,11 @@
+package Pertemuan3.Latihan2;
+
+/**
+ *
+ * @author Syafiq
+ */
+public class KonversiSuhu2 extends KonversiSuhu {
+    public double fahrenheitToReamur(double fahrenheit) {
+        return (fahrenheit - 32) * 4/9;
+    }
+}

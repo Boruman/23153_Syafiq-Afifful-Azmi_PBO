6a569a8ce20ed107c4c18087ff305a3a8bd7f883
Boruman <147920803+Boruman@users.noreply.github.com>
2024-11-03 18:11:03 +0700

    ‎

---
+++ Pertemuan 9/NilaiMhs.java
@@ -0,0 +1,11 @@
+package tugas9pbo;
+
+/**
+ *
+ * @author user
+ */
+public class NilaiMhs {
+    public double nilaiAkhir(int n1, int n2, int n3, int n4, int n5){
+        return (n1*0.1) + (n2*0.15) + (n3*0.25) + (n4*0.15) + (n5*0.35);
+    }
+}
